dd99823873d522e4f7eaa5d230e340047d6ea234
Matheus Lage <matheus.lage@icloud.com>
2024-01-22 05:07:36 -0300

    Criacao Controller e Save Function

--- src/main/java/com/example/springboot/repositories/ProductRepository.java
+++ src/main/java/com/example/springboot/repositories/ProductRepository.java
@@ -1,11 +1,12 @@
 package com.example.springboot.repositories;
 
+import com.example.springboot.models.ProductModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.UUID;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.springboot.models.ProductModel;
-
+@Repository
 public interface ProductRepository extends JpaRepository<ProductModel, UUID>{
 
 	
